fcf7fdfa06dc69e3a37f42607d561b2f13b796d3
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-07-18 21:16:03 -0700

    Update config.py

--- DaisyXMusic/config.py
+++ DaisyXMusic/config.py
@@ -1,21 +1,5 @@
-# DAISYXMUSIC- Telegram bot project
-# Copyright (C) 2021  Roj Serbest
-# Copyright (C) 2021  Inuka Asith
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-# Modified by Inukaasith
-
 import os
+from os import path
 from os import getenv
 
 from dotenv import load_dotenv
@@ -24,23 +8,23 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 que = {}
-SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "DaisyXupdates")
-BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/dcfdf612e499eef0e0b1f.png")
+SESSION_NAME = "AQC79eJ39IiE24rjPk0ASkdVI_LhtkCcQ43gjwvLxbm8aYE43INvs5Xvz4xce2JOs94kT1Mw8MH21vw7ThzOfsFbRVSLTPz0FS0aZgtOU8G3Gyjo8Mc8lJjq3hRf-gSfjWkzVjnPdTOABP3JbXptpk4EezyBTc5k_QZqZpSDWGGMuphjOgKVD1C2T1-7YT5TkW3VDN3YhVVygwyhbdHYTTRee6RzJPgwnEQ8ijNJBGWxpgNgd_j2rNuhwv2JO1GjBIqL8WXOEW1aCB5rJIF3k7sJk0GeeO6IQGFu_Y5aYuvOWeQXw5VEzSF47nFJsoHIomuq03Zx9RkIgpbUalmwiNLPTXKoywA"
+BOT_TOKEN = "1756652877:AAHGjb4XeqypEzZgR9OlSWfYdzmTZ3tj1ns"
+BOT_NAME = "@VCPlayBot"
+UPDATES_CHANNEL = "LaylaList"
+BG_IMAGE = "https://telegra.ph/file/9b13ea3ce046a1a5c8098.png"
 admins = {}
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "DaisyXhelper")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "DaisySupport_Official")
-PROJECT_NAME = getenv("PROJECT_NAME", "DaisyXMusic v4")
-SOURCE_CODE = getenv("SOURCE_CODE", "github.com/TeamOfDaisyX/DaisyXMusic")
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
-ARQ_API_KEY = getenv("ARQ_API_KEY", None)
-PMPERMIT = getenv("PMPERMIT", None)
-LOG_GRP = getenv("LOG_GRP", None)
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+API_ID = int("4042631")
+API_HASH = "e70b375fca15737a24eb5b8ba5ffe045"
+BOT_USERNAME = "ALMusicBot"
+ASSISTANT_NAME = "ALMusicAssistant"
+SUPPORT_GROUP = "Awesomesupport"
+PROJECT_NAME = "VCPlayBot2.0"
+SOURCE_CODE = "github.com/QueenArzoo/VCPlayBot"
+DURATION_LIMIT = int("14")
+ARQ_API_KEY = None
+PMPERMIT = "ENABLE"
+LOG_GRP = None
+COMMAND_PREFIXES = list("/")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+SUDO_USERS = list("797768146")
